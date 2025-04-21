ff74ecd2c95d1eb79015f118a9a7ff2bf894c90d
amandab99 <amanda9borsato@gmail.com>
2025-04-21 15:22:41 -0300

    Update mensagem.py

--- mensagem.py
+++ mensagem.py
@@ -1,6 +1,6 @@
 
 def mensagem_personalizada(nome):
-    return f"Olá, {nome}! Seja bem-vindo(a) ao seu primeiro projeto CI/CD no GitHub!!"
+    return f"Olá, {nome}!! Seja bem-vindo(a) ao seu primeiro projeto CI/CD no GitHub!!"
 
 if __name__ == '__main__':
     nome_usuario = input("Digite seu nome: ")
