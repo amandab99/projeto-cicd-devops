80fb1f85491f69bf65d9394f81cb5afb1c2fd8a1
amandab99 <amanda9borsato@gmail.com>
2025-04-21 15:22:59 -0300

    Update mensagem.py

--- mensagem.py
+++ mensagem.py
@@ -5,3 +5,4 @@ def mensagem_personalizada(nome):
 if __name__ == '__main__':
     nome_usuario = input("Digite seu nome: ")
     print(mensagem_personalizada(nome_usuario))
+# Este é um projeto CI/CD
