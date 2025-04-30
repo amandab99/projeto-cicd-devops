0e659a2b11e0a2b92b6b7064e4bd40c0b9c584f6
amandab99 <amanda9borsato@gmail.com>
2025-04-30 13:15:38 -0300

    Update mensagem.py

--- mensagem.py
+++ mensagem.py
@@ -1,9 +1,2 @@
-def saudacao(hora):
-    if 6 <= hora < 12:
-        return "Bom dia!"
-    elif 12 <= hora < 18:
-        return "Boa tarde!"
-    elif 18 <= hora <= 23:
-        return "Boa noite!"
-    else:
-        return "Boa madrugada!"
+def saudacao(nome):
+    return f"Olá, {nome}!"
